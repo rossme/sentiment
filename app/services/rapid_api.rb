cf01e65e12fa39db33c68046a46bacff1c65e879
Ross Buddie <r.buddie@gmail.com>
2020-10-07 00:20:23 +0100

    Add dotenv - Protect my secret data in .env file

--- app/services/rapid_api.rb
+++ app/services/rapid_api.rb
@@ -2,29 +2,34 @@ require 'uri'
 require 'net/http'
 require 'openssl'
 require 'json'
-require 'byebug'
 
-class RapidApiService
+
+class SentimentApi
+
     def self.parsing
-
-        url = URI("https://twinword-sentiment-analysis.p.rapidapi.com/analyze/")
+        url = URI("https://twinword-twinword-bundle-v1.p.rapidapi.com/sentiment_analyze/")
 
         http = Net::HTTP.new(url.host, url.port)
         http.use_ssl = true
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
         request = Net::HTTP::Post.new(url)
-        request["x-rapidapi-host"] = 'twinword-sentiment-analysis.p.rapidapi.com'
-        request["x-rapidapi-key"] = ENV['RAPID_API_KEY']
+        request["x-rapidapi-host"] = 'twinword-twinword-bundle-v1.p.rapidapi.com'
+        request["x-rapidapi-key"] = 'a5d18ad3eemshcdad0aeb6bdd35fp112bbdjsn47ff90eb1494'
         request["content-type"] = 'application/x-www-form-urlencoded'
-        request.body = "text=Hello World! What a fabulous day!"
+        request.body = "text=Holidays!"
 
         response = http.request(request)
         result = JSON.parse(response.read_body)
-
-
-        return result
-        
+        results = result["ratio"]
+        if results == 1
+            puts "You are happy!"
+            puts results
+        else
+            puts "Why so sad?"
+            puts results
+        end
     end
-    byebug
 end
+
+SentimentApi.parsing
