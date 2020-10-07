f73726e18a139b60b88d1100daad40e85f25fab3
Ross Buddie <r.buddie@gmail.com>
2020-10-07 13:08:54 +0100

    New branch

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,3 +1,16 @@
 class QuestionsController < ApplicationController
+    def new
+        @question = Question.new
+    end
 
+    def create
+        @question = Question.new(question_params)
+        @question.save
+    end
+
+    private
+
+    def question_params
+      params.require(:quesiton).permit(:description)
+    end
 end
