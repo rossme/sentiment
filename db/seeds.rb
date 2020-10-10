40b7c738cec0130f92c13cd64a218e1bb3d1c973
Ross Buddie <r.buddie@gmail.com>
2020-10-10 16:26:36 +0100

    API form

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,18 @@ puts "DESTROYING SEEDS..."
 Question.destroy_all
 puts "SEEDS DESTROYED"
 puts "SEEDING QUESTIONS"
-Question.create(description: 'How are you today?')
-Question.create(description: 'How do you feel?')
-Question.create(description: 'Did you sleep well?')
+
+data = [
+    { description: "How are you today?" },
+    { description: "How do you feel?" },
+    { description: "Did you sleep well last night?" },
+]
+
+data.each do | props |
+    question = Question.create!({
+        description: props[:description]
+    })
+end
+
 puts "FINISHED SEEDING"
+
